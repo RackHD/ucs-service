9c8279f094590e5b7b6ee4fb49fda1059077e464
Azzam Tannous <azzam.tannous@emc.com>
2017-02-15 13:34:48 -0800

    Add UCS Error handeling and negative unitTesting

--- controllers/default_controller.py
+++ controllers/default_controller.py
@@ -1,6 +1,7 @@
 # Copyright 2017, Dell EMC, Inc.
 
 from ucsmsdk.ucshandle import UcsHandle
+from ucsmsdk.ucsexception import UcsException
 
 
 def login_get(host=None, user=None, password=None):
@@ -9,6 +10,22 @@ def login_get(host=None, user=None, password=None):
         return handle.cookie
 
 
+def http_body_factory(function):
+    def wrapper(*args, **kwargs):
+        resp = function(*args, **kwargs)
+        if (len(resp) == 3):
+            http_res = {
+                "message": resp[0],
+                "status": resp[2],
+                "stack": resp[1]
+            }
+            return http_res, resp[2]
+        else:
+            return resp
+    return wrapper
+
+
+@http_body_factory
 def systemGetAll(host=None, user=None, password=None):
     handle = UcsHandle(host, user, password, secure=False)
     if handle.login():
@@ -19,71 +36,140 @@ def systemGetAll(host=None, user=None, password=None):
         finalObjs = {}
         for x in elements:
             units = []
-            components = handle.query_children(in_dn="sys", class_id=x["ciscoXmlName"])
-            for y in components:
-                subElement = {"relative_path": "/" + (vars(y))["dn"]}
-                units.append(subElement)
-            finalObjs[x["humanReadableName"]] = units
-            handle.logout()
+            try:
+                components = handle.query_children(in_dn="sys", class_id=x["ciscoXmlName"])
+            except UcsException as e:
+                handle.logout()
+                return 'Inrternal Server Error', e.error_descr, 500
+            else:
+                if(type(components) == list):
+                    for y in components:
+                        subElement = {"relative_path": "/" + (vars(y))["dn"]}
+                        units.append(subElement)
+                    finalObjs[x["humanReadableName"]] = units
+                    handle.logout()
+                else:
+                    handle.logout()
+                    return "Couldn't fetch " + x["ciscoXmlName"], "", 500
         return finalObjs
+    else:
+        handle.logout()
+        return 'Forbiden', "", 403
 
 
+@http_body_factory
 def getRackmount(host=None, user=None, password=None):
     data = []
     handle = UcsHandle(host, user, password, secure=False)
     if handle.login():
-        computeRackUnit = handle.query_children(in_dn="sys", class_id="computeRackUnit")
-        for x in computeRackUnit:
-            server = {}
-            server["name"] = x.rn
-            server["path"] = x.dn
-            server["macs"] = []
-            macs = handle.query_children(in_dn=x.dn, class_id='PciEquipSlot')
-            for y in macs:
-                server["macs"].append(y.mac_left)
-                server["macs"].append(y.mac_right)
-            data.append(server)
+        try:
+            computeRackUnit = handle.query_children(in_dn="sys", class_id="computeRackUnit")
+        except UcsException as e:
             handle.logout()
-        return data
+            return 'Inrternal Server Error', e.error_descr, 500
+        else:
+            if (type(computeRackUnit) == list):
+                for x in computeRackUnit:
+                    server = {}
+                    server["name"] = x.rn
+                    server["path"] = x.dn
+                    server["macs"] = []
+                    try:
+                        macs = handle.query_children(in_dn=x.dn, class_id='PciEquipSlot')
+                    except UcsException as e:
+                        handle.logout()
+                        return 'Inrternal Server Error', e.error_descr, 500
+                    for y in macs:
+                        server["macs"].append(y.mac_left)
+                        server["macs"].append(y.mac_right)
+                    data.append(server)
+                    handle.logout()
+                    return data
+            else:
+                handle.logout()
+                return "Couldn't fetch computeRackUnits:", "", 500
+
+    else:
+        handle.logout()
+        return 'Forbiden', "", 403
 
 
+@http_body_factory
 def getCatalog(host=None, user=None, password=None, identifier=None):
     data = []
 
     handle = UcsHandle(host, user, password, secure=False)
     if handle.login():
-        elements = handle.query_children(in_dn=identifier)
-        for element in elements:
-            data.append(reduce(element.__dict__))
+        try:
+            elements = handle.query_children(in_dn=identifier)
+        except UcsException as e:
+            handle.logout()
+            return 'Inrternal Server Error', e.error_descr, 500
+        else:
+            if (type(elements) == list):
+                for element in elements:
+                    data.append(reduce(element.__dict__))
+                handle.logout()
+                return data
+            else:
+                handle.logout()
+                return "Couldn't fetch " + identifier, "", 500
+    else:
         handle.logout()
-        return data
+        return 'Forbiden', "", 403
 
 
+@http_body_factory
 def getChassis(host=None, user=None, password=None):
     data = []
 
     handle = UcsHandle(host, user, password, secure=False)
     if handle.login():
-        elememts = handle.query_children(in_dn="sys", class_id="EquipmentChassis")
-        for element in elememts:
-            chassis = {}
-            identifier = element.dn
-            obj = handle.query_dn(dn=identifier)
-            chassis["name"] = obj.rn
-            chassis["path"] = obj.dn
-            chassis["members"] = []
-            blades = handle.query_children(in_dn=identifier, class_id='ComputeBlade')
-            for x in blades:
-                server = {}
-                server["name"] = x.rn
-                server["path"] = x.dn
-                server["macs"] = []
-                adptares = handle.query_children(in_dn=x.dn, class_id='AdaptorUnit')
-                for x in adptares:
-                    server["macs"].append(x.base_mac)
-                chassis["members"].append(server)
-            data.append(chassis)
+        try:
+            elememts = handle.query_children(in_dn="sys", class_id="EquipmentChassis")
+        except UcsException as e:
+            return 'Inrternal Server Error', e.error_descr, 500
+        else:
+            if (type(elememts) == list):
+                for element in elememts:
+                    chassis = {}
+                    identifier = element.dn
+                    obj = handle.query_dn(dn=identifier)
+                    chassis["name"] = obj.rn
+                    chassis["path"] = obj.dn
+                    chassis["members"] = []
+                    try:
+                        blades = handle.query_children(in_dn=identifier, class_id='ComputeBlade')
+                    except UcsException as e:
+                        handle.logout()
+                        return 'Inrternal Server Error', e.error_descr, 500
+                    else:
+                        if (type(blades) == list):
+                            for x in blades:
+                                server = {}
+                                server["name"] = x.rn
+                                server["path"] = x.dn
+                                server["macs"] = []
+                                try:
+                                    adptares = handle.query_children(in_dn=x.dn, class_id='AdaptorUnit')
+                                except UcsException as e:
+                                    handle.logout()
+                                    return 'Inrternal Server Error', e.error_descr, 500
+                                else:
+                                    for x in adptares:
+                                        server["macs"].append(x.base_mac)
+                                    chassis["members"].append(server)
+                                    data.append(chassis)
+                        else:
+                            handle.logout()
+                            return "Couldn't fetch ComputeBlade", "", 500
+            else:
+                handle.logout()
+                return "Couldn't fetch EquipmentChassis", "", 500
+    else:
         handle.logout()
+        return 'Forbiden', "", 403
+    handle.logout()
     return data
 
 
