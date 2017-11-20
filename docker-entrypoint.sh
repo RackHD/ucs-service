d5ff3d509d39f9995a36068020929bcbb3f0e985
mcgg <zhaokaihao@gmail.com>
2017-11-20 08:28:54 -0500

    fix docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,5 +2,5 @@
 # Copyright 2017, Dell EMC, Inc.
 
 service rabbitmq-server start
-python app.py >> app.log &
-python tasks.py worker >> tasks.log
+python app.py >> app.log 2>&1 &
+python tasks.py worker >> tasks.log 2>&1
